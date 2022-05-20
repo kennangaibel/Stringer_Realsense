e75a4a33e69a26e7e2b00904ba27a09b12f3c2d6
Kennan Gaibel <kgaibel@cs.washington.edu>
2022-05-20 12:06:56 -0700

    get_corners refined and filters out correct corners

---
+++ Deconstructed Programs/cvTest.py
@@ -0,0 +1,31 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+from PIL import Image
+
+img = cv2.imread('ex.JPG')
+# img = cv2.imread('ex.JPG')
+
+# resize image
+scale_percent = 20  # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+# resize image
+img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,5,3,0.04)
+ret, dst = cv2.threshold(dst,0.1*dst.max(),255,0)
+dst = np.uint8(dst)
+ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+for i in range(1, len(corners)):
+    print(corners[i])
+img[dst>0.1*dst.max()]=[0,0,255]
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
--- Deconstructed Programs/get_corners.py
+++ Deconstructed Programs/get_corners.py
@@ -2,9 +2,11 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 from PIL import Image
-from matplotlib import pyplot as plt
-# WORKING PROGRAM THAT TAKES A RECORDED BAG FILE AND DETECTS CORNERS
 
+MIN_DEPTH = 0.4
+MAX_DEPTH = 1.0
+
+# WORKING PROGRAM THAT TAKES A RECORDED BAG FILE AND DETECTS CORNERS
 
 # Creates Pipeline
 pipeline = rs.pipeline()
@@ -12,30 +14,27 @@ pipeline = rs.pipeline()
 config = rs.config()
 # Tell config that we will use a recorded device from file to be used by the pipeline through playback.
 # Allows us to use the bag file created by save_single_frameset
-# !: Can test with Frameset 97.bag
 rs.config.enable_device_from_file(config,'./test.bag')
 pipeline.start(config)
 
-# Converts bag file to numpy image that opencv can use
+
 # Wait for a coherent pair of frames: depth and color
 frames = pipeline.wait_for_frames()
+# Converts bag file to numpy of depth data
 depth_frame = frames.get_depth_frame()
+# Converts bag file to numpy image that opencv can use
 color_frame = frames.get_color_frame()
 
-# Convert images to numpy arrays
+# Convert images to numpy arrays that opencv can use
 depth_image = np.asanyarray(depth_frame.get_data())
 color_image = np.asanyarray(color_frame.get_data())
-
-
 
 # Gets numpy array color_image and converts it to a png that opencv can use
 im = Image.fromarray(color_image)
 # What to save the image as
 im.save('stringer_image.png')
 
-# read the image
-# load image
-# Read image
+# Loads image and runs opencv corner detection software
 img = cv2.imread('stringer_image.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = np.float32(gray)
@@ -45,8 +44,27 @@ dst = np.uint8(dst)
 ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
 corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+# !: ADD DEPTH FILTER HERE
+# Prints out pixel coordinates of all detected corners
+
+# !!: PROBLEM IS THAT BAG FILE IS IN 640, 480 RESOLUTION
+# !!: NEED TO SOMEHOW CONVERT OR GET IT IN THE SAME RESOLUTION
+# Solutionn: Divide the pixel value, so 1920 / 640 = 3
 for i in range(1, len(corners)):
-    print(corners[i])
+    # Gets the pixel coordinates of the corner
+    x_pixel = int(corners[i][0] / 3)
+    y_pixel = int(corners[i][1] / 3)
+    # print(x_pixel)
+    # print(y_pixel)
+    # If the corner is too far away, don't include it
+    if (depth_image[y_pixel][x_pixel] > MIN_DEPTH and
+        depth_image[y_pixel][x_pixel] < MAX_DEPTH):
+        # corners.pop(i)
+        print(corners[i])
+
+# for i in range(1, len(corners)):
+#     print(corners[i])
+
 img[dst>0.1*dst.max()]=[0,0,255]
 cv2.imshow('image', img)
 cv2.waitKey(0)
